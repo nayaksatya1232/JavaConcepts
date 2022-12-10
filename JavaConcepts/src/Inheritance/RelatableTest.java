f88b6e10ba55d8313f3fa5d7b2266e412e91b530
nayaksatya1232 <nayaksatya1232@gmail.com>
2022-12-10 18:16:38 +0530

    TestCase Modified

--- JavaConcepts/src/Inheritance/RelatableTest.java
+++ JavaConcepts/src/Inheritance/RelatableTest.java
@@ -2,7 +2,7 @@ package Inheritance;
 
 public class RelatableTest {
 	public static void main(String[] args) {
-		Student std1 = new Student(25,"satya");
+		Student std1 = new Student(25,"Satya");
 		Student std2 = new Student(22,"Rahul");
 		System.out.println("SMALLER"+CompareObject.smaller(std1, std2).toString());
 		System.out.println("BIGGER"+CompareObject.larger(std1, std2).toString());
